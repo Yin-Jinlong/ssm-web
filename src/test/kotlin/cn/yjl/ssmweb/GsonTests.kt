bae73c41931d569814b587ae9d158fefb41c68f6
yjl <yjl_1567@qq.com>
2023-09-11 18:25:56 +0800

    添加全局单例Bean gson test实例从bean获取 移动内存文件管理器到全局bean

--- src/test/kotlin/cn/yjl/ssmweb/GsonTests.kt
+++ src/test/kotlin/cn/yjl/ssmweb/GsonTests.kt
@@ -2,15 +2,20 @@ package cn.yjl.ssmweb
 
 import com.google.gson.Gson
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 
+@SpringBootTest
 class GsonTests {
+
+    @Autowired
+    lateinit var gson:Gson
 
     /**
      * 测试Gson是否正常
      */
     @Test
     fun testGson() {
-        val gson = Gson()
         val gt=gson.fromJson("{name:'gson',age:20}",GsonTestClass::class.java)
         assert(gt.name=="gson")
         assert(gt.age==20)
