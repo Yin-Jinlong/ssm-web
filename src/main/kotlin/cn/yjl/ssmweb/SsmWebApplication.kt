e69c058989b594d8f0c2802b76a74a7694b92313
yjl <yjl_1567@qq.com>
2023-09-18 18:14:55 +0800

    提取gz自动对应acceptEncoding打开文件

--- src/main/kotlin/cn/yjl/ssmweb/SsmWebApplication.kt
+++ src/main/kotlin/cn/yjl/ssmweb/SsmWebApplication.kt
@@ -1,21 +1,16 @@
 package cn.yjl.ssmweb
 
+import cn.yjl.io.MemFile
 import cn.yjl.io.MemFileManager
 import cn.yjl.log.util.getLogger
-import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.servlet.http.HttpServletResponse.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.core.io.ClassPathResource
-import org.springframework.core.io.Resource
 import org.springframework.http.HttpHeaders
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -44,27 +39,29 @@ class SsmWebApplication {
             }
         }
         val acceptEncoding: String? = req.getHeader(HttpHeaders.ACCEPT_ENCODING)
-        if (acceptEncoding != null && acceptEncoding.contains("gzip")) {
-            val gz = memFileManager["$path.gz"]
-            gz?.apply {
-                resp.status = SC_OK
-                resp.addHeader(HttpHeaders.CONTENT_ENCODING, "gzip")
-                resp.setHeader(HttpHeaders.CONTENT_TYPE, req.servletContext.getMimeType(path))
-                req.log(this.filePath)
-                writeTo(resp)
-                return
-            }
-        }
-        memFileManager[path]?.apply {
+        path.openMemFileForEncoding(resp, acceptEncoding)?.apply {
             resp.status = SC_OK
             resp.setHeader(HttpHeaders.CONTENT_TYPE, req.servletContext.getMimeType(path))
             writeTo(resp)
-            req.log(this.filePath)
+            req.log(filePath)
             return
         }
-
         resp.status = SC_NOT_FOUND
         req.log(SC_NOT_FOUND)
+    }
+
+    /**
+     * 打开请求的文件，如果gz存在则返回gz文件
+     */
+    private fun String.openMemFileForEncoding(resp: HttpServletResponse, acceptEncoding: String?): MemFile? {
+        if (acceptEncoding != null && acceptEncoding.contains("gzip")) {
+            val gz = memFileManager["$this.gz"]
+            gz?.apply {
+                resp.addHeader(HttpHeaders.CONTENT_ENCODING, "gzip")
+                return this
+            }
+        }
+        return memFileManager[this]
     }
 }
 
