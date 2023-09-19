b2cc55b34c87003f819fead58db2f6627727f4ac
yjl <yjl_1567@qq.com>
2023-09-19 14:22:39 +0800

    修改项目结构

---
+++ server/build.gradle.kts
@@ -0,0 +1,88 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    war
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+}
+
+rootProject.apply {
+    group = "cn.yjl"
+    version = "0.0.1-SNAPSHOT"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://maven.aliyun.com/repository/public")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/central")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/gradle-plugin/")
+    }
+    mavenCentral()
+}
+
+configurations.all {
+    exclude("com.zaxxer", "HikariCP")
+}
+
+dependencies {
+    // spring-boot-jdbc
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    // spring-boot-web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    // druid
+    // https://mvnrepository.com/artifact/com.alibaba/druid-spring-boot-starter
+    implementation("com.alibaba:druid-spring-boot-starter:1.2.19")
+
+    // spring-validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // servlet-api
+    implementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
+
+    // mybatis
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.2")
+
+    // gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // mysql
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.build {
+    doLast {
+        copy {
+            from("src/main/webapp")
+            into("build/resources/main")
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
 rootProject.name = "ssm-web"
+
+include("api")
+include("server")
+include("web")
