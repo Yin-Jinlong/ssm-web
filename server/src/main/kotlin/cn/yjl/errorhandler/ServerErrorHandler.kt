0a7f663c3f5538d0effb7d46a4968c20f7000110
yjl <yjl_1567@qq.com>
2023-10-10 15:59:20 +0800

    优化代码

--- server/src/main/kotlin/cn/yjl/errorhandler/ServerErrorHandler.kt
+++ server/src/main/kotlin/cn/yjl/errorhandler/ServerErrorHandler.kt
@@ -4,7 +4,6 @@ import cn.yjl.log.util.getLogger
 import cn.yjl.resp.ErrorRespJson
 import cn.yjl.resp.RespCode
 import cn.yjl.resp.ResponseJson
-import cn.yjl.util.isNull
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
@@ -29,7 +28,7 @@ class ServerErrorHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Throwable::class)
     fun handleValidationException(e: Throwable, resp: HttpServletResponse): ResponseJson {
-        if (e.cause.isNull())
+        if (e.cause == null)
             LOGGER.severe(e.message)
         else
             LOGGER.severe(e.message + "-> ${e.cause?.message}")
--- server/src/main/kotlin/cn/yjl/util/AnyUtil.kt
+++ server/src/main/kotlin/cn/yjl/util/AnyUtil.kt
@@ -5,7 +5,3 @@ inline fun Any.sync(block: () -> Unit) {
         block()
     }
 }
-
-inline fun Any?.isNull() = this == null
-
-inline fun Any?.isNotNull() = this != null
